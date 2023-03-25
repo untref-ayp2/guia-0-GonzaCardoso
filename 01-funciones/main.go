7709e5e78216d1915b5f7bdff2fd73d5be1085c3
gonza <cardoso63263@estudiantes.edu.ar>
2023-03-25 18:18:30 -0300

    optimizando

--- 01-funciones/main.go
+++ 01-funciones/main.go
@@ -5,23 +5,18 @@ import (
 )
 
 func main() {
-	var funcion []float64 = []float64{3.43, -4.00, -0.52, 0.00, 0.03, 1.50}
+	var funcion []float32 = []float32{3.43, -4.00, -0.52, 0.00, 0.03, 1.50}
 	var i int = 0
-	var h int = 1
 	fmt.Print(funcion[0])
 	for i < len(funcion)-1 {
-		if funcion[h] < 0.0 {
-			fmt.Print(" ", funcion[h], "*x^", h)
+		if funcion[i+1] < 0.0 {
+			fmt.Print(" ", funcion[i+1], "*x^", i+1)
 			i++
-			h++
-		} else if funcion[h] == 0.0 {
+		} else if funcion[1+1] == 0.0 {
 			i++
-			h++
 		} else {
-			fmt.Print(" +", funcion[h], "*x^", h)
+			fmt.Print(" +", funcion[i+1], "*x^", i+1)
 			i++
-			h++
-
 		}
 	}
 }
